4aa5074b26801792ae8580b5eadc402231c9f3dd
crysty72 <crysty_72@hotmail.com>
2023-11-11 20:34:30 -0300

    ejercicio-3-react

---
+++ src/componentes/ProductDetail.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+function ProductDetail() {
+  return (
+    <div className="product-detail">
+      <h1>Nombre del Producto</h1>
+      <p>Descripción del producto</p>
+      <p>Precio: $100.00</p>
+      <p>SKU: SKU12345</p>
+      <p>Cantidad disponible: 50 unidades</p>
+    </div>
+  );
+}
+
+export default ProductDetail;
